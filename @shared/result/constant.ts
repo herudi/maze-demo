5c4ad50ca05e29a1a85ea0aaa511f72dbad3e549
herudi <herudi7@gmail.com>
2022-04-23 14:57:26 +0700

    asset2

--- @shared/result/constant.ts
+++ @shared/result/constant.ts
@@ -1,2 +1,2 @@
-export const BUILD_ID: string = '1650698408125';
+export const BUILD_ID: string = '1650700631934';
 export const ENV: string = 'production';
--- netlify/edge-functions/maze-ssr.js
+++ netlify/edge-functions/maze-ssr.js
@@ -1,5 +1,3 @@
 import maze from "../../maze.build.js";
 
-const asset_url = new URL("public", import.meta.url).href.replace("/netlify/edge-functions");
-
-export default (request, context) => maze(asset_url).handleEvent({ request, context });
+export default (request, context) => maze.handleEvent({ request, context });
